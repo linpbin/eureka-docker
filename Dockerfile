dac24db33e97b22eaa025c1807cc870d08d792f6
linpbin <linpbin@dingtalk.com>
2020-02-28 11:02:34 +0800

    finsh

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM java:8
 RUN /bin/cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo 'Asia/Shanghai' >/etc/timezone 
 VOLUME  /tmp 
-ADD eureka-docker-0.0.1-SNAPSHOT.jar app.jar
+COPY /target/eureka-docker-0.0.1-SNAPSHOT.jar app.jar
 RUN sh -c 'touch /app.jar'
 ENV JAVA_OPTS=""
 EXPOSE 8761
--- start.sh
+++ start.sh
@@ -4,30 +4,27 @@ cd `dirname $0`
 img_mvn="maven:3.3.3-jdk-8"                 # docker image of maven
 m2_cache=~/.m2                              # the local maven cache dir
 proj_home=$PWD                              # the project root dir
-img_output="eureka"      # output image tag
-
-git pull  # should use git clone https://name:pwd@xxx.git
-
+img_output="eureka/eureka-server"      # output image tag
 echo "use docker maven"
 docker run --rm \
    -v $m2_cache:/root/.m2 \
    -v $proj_home:/usr/src/mymaven \
-   -w /usr/src/mymaven $img_mvn mvn clean package -U -Pdev -DskipTests
-
-docker build -t $img_output .
-
-mkdir -p $PWD/logs
-chmod 777 $PWD/logs
-
-# 删除容器
-docker rm -f docker-eureka &> /dev/null
-
-version=`date "+%Y%m%d%H"`
-
-# 启动镜像
-docker run -d --restart=on-failure:5 --privileged=true \
-    -w /home \
-    -v $PWD/logs:/home/logs \
-    --net=host \
-    --name docker-eureka docker-eureka \
-    java -jar /app.jar
+   -w /usr/src/mymaven $img_mvn mvn clean package -U -DskipTests
+docker-compose down
+#docker build -t $img_output .
+docker-compose up -d
+#mkdir -p $PWD/logs
+#chmod 777 $PWD/logs
+#
+## 删除容器
+#docker rm -f docker-eureka &> /dev/null
+#
+#version=`date "+%Y%m%d%H"`
+#
+## 启动镜像
+#docker run -d --restart=on-failure:5 --privileged=true \
+#    -w /home \
+#    -v $PWD/logs:/home/logs \
+#    --net=host \
+#    --name docker-eureka eureka/eureka-server \
+#    java -jar /app.jar
