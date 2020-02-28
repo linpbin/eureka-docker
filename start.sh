05b810d9a0873240fa157d746adf818fc310fce4
linpbin <linpbin@dingtalk.com>
2020-02-28 11:06:55 +0800

    finsh

--- start.sh
+++ start.sh
@@ -10,21 +10,19 @@ docker run --rm \
    -v $m2_cache:/root/.m2 \
    -v $proj_home:/usr/src/mymaven \
    -w /usr/src/mymaven $img_mvn mvn clean package -U -DskipTests
-docker-compose down
-#docker build -t $img_output .
-docker-compose up -d
-#mkdir -p $PWD/logs
-#chmod 777 $PWD/logs
-#
-## 删除容器
-#docker rm -f docker-eureka &> /dev/null
-#
-#version=`date "+%Y%m%d%H"`
-#
-## 启动镜像
-#docker run -d --restart=on-failure:5 --privileged=true \
-#    -w /home \
-#    -v $PWD/logs:/home/logs \
-#    --net=host \
-#    --name docker-eureka eureka/eureka-server \
-#    java -jar /app.jar
+docker build -t $img_output .
+mkdir -p $PWD/logs
+chmod 777 $PWD/logs
+
+# 删除容器
+docker rm -f docker-eureka &> /dev/null
+
+version=`date "+%Y%m%d%H"`
+
+# 启动镜像
+docker run -d --restart=on-failure:5 --privileged=true \
+    -w /home \
+    -v $PWD/logs:/home/logs \
+    --net=host \
+    --name docker-eureka eureka/eureka-server \
+    java -jar /app.jar
